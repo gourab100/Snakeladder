c94b5be1634c30f149407196b51ee76a47b8926a
gourab <gourab.saheb100@gmail.com>
2022-06-17 19:56:57 +0530

    ADD: Welcome to Snake and Ladder game

---
+++ SnakeLedder/Program.cs
@@ -0,0 +1,7 @@
+﻿using SnakeLedder;
+
+
+
+Console.WriteLine("   Snake and ladder game   ");
+Snakeladder Start = new Snakeladder();
+Start.Start();
---
+++ SnakeLedder/Snakeladder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLedder
+{
+    internal class Snakeladder
+    {
+        public void Start()
+        {
+            int position = 0;
+            int playerone;
+            playerone = position;
+            Console.WriteLine("playerone position " + playerone);
+        }
+
+    }
+}
