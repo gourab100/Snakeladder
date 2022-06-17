fd44d8dc6485b82eee3529ad0f2d1ae2490e541a
gourab <gourab.saheb100@gmail.com>
2022-06-17 21:45:16 +0530

    JUST WIN THE GAME

--- SnakeLedder/Snakeladder.cs
+++ SnakeLedder/Snakeladder.cs
@@ -8,19 +8,73 @@ namespace SnakeLedder
 {
     internal class Snakeladder
     {
+        const int WIN_POSITION = 100;
+        const int START_POSITION = 0;
+
         public void Start()
         {
-            int position;
-            int playerOne = 3;    
-            int checkwin;
-            Console.WriteLine("player One position is " + playerOne);
-            while (playerOne <= 100)
+            int player;
+            int playerOne = START_POSITION, playerTwo = START_POSITION;    
+            Console.WriteLine($"player One position is " + playerOne); 
+            Console.WriteLine($"player Two position is "+ playerTwo); 
+            
+            int turn = 1; 
+            while (true) 
             {
-                
-                checkwin = CheckWin(playerOne);
-                if (checkwin == 1) 
+                if (turn == 1) 
                 {
-                    Console.WriteLine("playerOne wins!!");
+                    Console.WriteLine(" PLAYER ONE TURN");
+                    player = PlayGame(playerOne, turn); 
+                    turn = 0; 
+                    if (player > playerOne) 
+                    {
+                        turn = 1;
+                    }
+                    playerOne = player; 
+                }
+                if (playerOne == WIN_POSITION) 
+                {
+                    Console.WriteLine("  PLAYER ONE WINS  ");
+                    break; // player one wins the game
+                }
+                if (turn == 0)
+                {
+                    
+                    Console.WriteLine(" PLAYER TWO TURN");
+                    player = PlayGame(playerTwo, turn);
+                    turn = 1; 
+                    if (player > playerTwo) 
+                    {
+                        turn = 0;
+                    }
+                    playerTwo = player; 
+                }
+                if (playerTwo == WIN_POSITION) 
+                {
+                    Console.WriteLine("   PLAYER TWO WINS   ");
+                    break; //player 2 wins the game
+                }
+            }
+        }
+
+
+
+        public int PlayGame( int playerposition, int turn)
+        {
+            int position;    
+            int checkwin;
+            while (playerposition != WIN_POSITION) 
+            {
+               
+                checkwin = CheckWin(playerposition);
+                if (checkwin == 1 && turn == 1) 
+                {
+                    Console.WriteLine("player One wins!!");
+                    break; 
+                }
+                if (checkwin == 1 && turn == 0) 
+                {
+                    Console.WriteLine("player Two wins!!");
                     break; 
                 }
                 if (checkwin == 2) 
@@ -35,23 +89,23 @@ namespace SnakeLedder
                 if (position == 0)
                 {
                     Console.WriteLine("its a no play");
-                    playerOne += position;
+                    playerposition += position;
                 }
 
                 //snake condition
-                if (playerOne == 0 && position < 0)  
+                if (playerposition == 0 && position < 0)   
                 {
                     Console.WriteLine("its a snake bite @ 0");
-                    playerOne = 0; 
+                    playerposition = 0; 
                 }
-                if (playerOne > 0 && position < 0)
+                if (playerposition > 0 && position < 0)
                 {
                     Console.WriteLine("its a snake bite");
-                    playerOne += position; 
-                    if (playerOne < 0)
+                    playerposition += position; 
+                    if (playerposition < 0)
                     {
                         
-                        playerOne = 0;
+                        playerposition = 0;
                     }
                 }
 
@@ -59,28 +113,42 @@ namespace SnakeLedder
                 if (position > 0)
                 {
                     Console.WriteLine("its a ladder"); 
-                    playerOne += position;
+                    playerposition += position;
                 }
-                if (playerOne > 100)
+                if (playerposition > WIN_POSITION)
                 {
-                    playerOne -= position; 
+                    playerposition -= position; 
                 }
-                Console.WriteLine($"player One rolls die and get position "+ playerOne);
+                if (turn == 1) 
+                {
+                    Console.WriteLine("Player One rolls die and gets position " + playerposition);
+                    break;
+                }
+                if (turn == 0) 
+                {
+                    Console.WriteLine($"Player Two rolls die and get position " + playerposition);
+                    break;
+                }
             }
+            return playerposition; 
         }
+
+
+
+
 
         public int CheckWin(int playerOne)
         {
-            if (playerOne == 100) 
+            if (playerOne == WIN_POSITION) 
                 return 1;
-            if (playerOne > 100) 
+            if (playerOne > WIN_POSITION) 
                 return 2;
             else 
                 return 0;
         }
 
 
-         Random random = new Random();
+        Random random = new Random();
         int countdice = 0;
         public int RollDiec()
         {
